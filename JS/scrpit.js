4bfa76129c0fe8eb70cddea23da2fe98891459b6
otavio azevedo <otavioazevedo39@gmail.com>
2024-02-17 18:09:25 -0300

    ,,

--- JS/scrpit.js
+++ JS/scrpit.js
@@ -1,19 +1,38 @@
-// 1 number
-console.log(typeof 2);
-console.log(typeof 2.14);
-console.log(typeof -127);
+// // 1 number
+// console.log(typeof 2);
+// console.log(typeof 2.14);
+// console.log(typeof -127);
 
 
 
-// operaçãoes artimeticas
+// // operaçãoes artimeticas
 
-console.log(2 + 4);
-console.log(10 - 5);
-console.log(10 * 5);
-console.log(10 / 5);
+// console.log(2 + 4);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(10 / 5);
 
-// special numbers
+// // special numbers
 
-console.log(typeof Infinity)
-console.log(typeof -Infinity)
-console.log( typeof NaN)
+// console.log(typeof Infinity)
+// console.log(typeof -Infinity)
+// console.log( typeof NaN)
+
+
+// // strings 
+
+// console.log("textos")
+// console.log('mais um textp')
+// console.log (typeof '13')
+
+
+// // simbolos especiais em string
+
+// console.log ("testandp a \n quebra de linha")
+// cocatenação 
+
+console.log ("oi," + "tudo bem?,")
+
+// templates strings 
+
+console.log(`a soma de 2 + 2 é: ${2 + 2}`)
