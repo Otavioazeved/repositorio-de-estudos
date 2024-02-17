1a94a84df12db468e667106fc6449d1818d095ec
otavio azevedo <otavioazevedo39@gmail.com>
2024-02-17 15:38:00 -0300

    ..

--- JS/scrpit.js
+++ JS/scrpit.js
@@ -1,1 +1,20 @@
-console.log("testando js 2")
+// 1 number
+console.log(typeof 2);
+console.log(typeof 2.14);
+console.log(typeof -127);
+
+
+
+// operaçãoes artimeticas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+
+// special numbers
+
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * asd );
+console.log( typeof NaN);
