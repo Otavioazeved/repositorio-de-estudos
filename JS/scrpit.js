62e928e6d1815a40a8f0253bf115b821f9825b0f
otavio azevedo <otavioazevedo39@gmail.com>
2024-02-18 10:14:39 -0300

    ..

--- JS/scrpit.js
+++ JS/scrpit.js
@@ -31,13 +31,35 @@
 // console.log ("testandp a \n quebra de linha")
 // cocatenação 
 
-console.log ("oi," + "tudo bem?,")
+// console.log ("oi," + "tudo bem?,")
 
-// templates strings 
+// // templates strings 
 
-console.log(`a soma de 2 + 2 é: ${2 + 2}`)
+// console.log(`a soma de 2 + 2 é: ${2 + 2}`)
 
 
-// cocatenação dnv
+// // cocatenação dnv
 
-console.log(" oi, "  + "tudo bem?")
+// console.log(" oi, "  + "tudo bem?")
+
+// // booleano
+
+
+// console.log(true)
+// console.log(5 >20 )
+// console.log(4<3)
+// console.log(typeof false)
+
+// // comparações
+
+// console.log (5 <= 5)
+// console.log( 10 === 10)
+// console.log( 10 === 9)
+
+//  operadores logicos
+
+console.log(5 > 4 && 5 < 2 )
+
+console.log( !false)
+
+console.log  (50 < 100 && 50 >= 3)
