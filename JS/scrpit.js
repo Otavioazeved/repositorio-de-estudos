ec6b52c358e70e315f938a2533a1dd6a728ebfbe
otavio azevedo <otavioazevedo39@gmail.com>
2024-02-26 19:31:32 -0300

    ..

--- JS/scrpit.js
+++ JS/scrpit.js
@@ -1,29 +1,45 @@
-// while 
+// // while 
 
-let p = 0
+// let p = 0
 
-while( p < 5 ) {
-    console.log (`repetindo  ${p}`)
-    p = p + 1;
+// while( p < 5 ) {
+//     console.log (`repetindo  ${p}`)
+//     p = p + 1;
+// }
+
+
+// // do whiile 
+
+// let o = 10 
+// do {
+//     console.log(`valor de o: ${o}`)
+//     o--
+// } while (o > 1);
+
+
+
+// // for
+// for (let t = 0; t < 10; t++) {c++
+//     console.log(`repetindo algo..`)
+// }
+//  let t = 10
+
+//  for(t; t > 0; t = t - 1) {
+//     console.log( "repetindo algo ")
+//  }
+// console.log("ola , tudo bem?")
+// console.log("ola , tudo bem?")
+// console.log("ola , tudo bem?")
+// console.log("ola , tudo bem?")
+// console.log("ola , tudo bem?")
+// console.log("ola , tudo bem?")
+
+
+
+
+var c = 1 
+while(c < 6) {
+    console.log("ola tudo bem ")
+    c++
+
 }
-
-
-// do whiile 
-
-let o = 10 
-do {
-    console.log(`valor de o: ${o}`)
-    o--
-} while (o > 1);
-
-
-
-// for
-for (let t = 0; t < 10; t++) {
-    console.log(`repetindo algo..`)
-}
- let t = 10
-
- for(t; t > 0; t = t - 1) {
-    console.log( "repetindo algo ")
- }
