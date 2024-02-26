c0715e108d1a33e834fc47511f4f45a1ad76578d
otavio azevedo <otavioazevedo39@gmail.com>
2024-02-26 18:27:41 -0300

    ..

---
+++ JS/scrpit.js
@@ -0,0 +1,17 @@
+// while 
+
+let p = 0
+
+while( p < 5 ) {
+    console.log (`repetindo  ${p}`)
+    p = p + 1;
+}
+
+
+// loop infinito
+
+let x = 10
+
+while (x > 5) {
+    console.log ( `imprimindo ${x}`)
+}
