13d72a1c6782e25187853c82b6c81580382f1b5d
otavio azevedo <otavioazevedo39@gmail.com>
2024-02-27 13:27:06 -0300

    ..

--- JS/scrpit.js
+++ JS/scrpit.js
@@ -1,96 +1,96 @@
-// // while 
+// // // while 
 
-// let p = 0
+// // let p = 0
 
-// while( p < 5 ) {
-//     console.log (`repetindo  ${p}`)
-//     p = p + 1;
-// }
-
-
-// // do whiile 
-
-// let o = 10 
-// do {
-//     console.log(`valor de o: ${o}`)
-//     o--
-// } while (o > 1);
+// // while( p < 5 ) {
+// //     console.log (`repetindo  ${p}`)
+// //     p = p + 1;
+// // }
 
 
+// // // do whiile 
 
-// // for
-// for (let t = 0; t < 10; t++) {c++
-//     console.log(`repetindo algo..`)
-// }
-//  let t = 10
-
-//  for(t; t > 0; t = t - 1) {
-//     console.log( "repetindo algo ")
-//  }
-// console.log("ola , tudo bem?")
-// console.log("ola , tudo bem?")
-// console.log("ola , tudo bem?")
-// console.log("ola , tudo bem?")
-// console.log("ola , tudo bem?")
-// console.log("ola , tudo bem?")
+// // let o = 10 
+// // do {
+// //     console.log(`valor de o: ${o}`)
+// //     o--
+// // } while (o > 1);
 
 
 
+// // // for
+// // for (let t = 0; t < 10; t++) {c++
+// //     console.log(`repetindo algo..`)
+// // }
+// //  let t = 10
 
-// var c = 1 
-// while (c <= 6) {
-//     console.log(`passo ${c}`)
-//     c++
-
-// }
-
-
-
-// var c = 0
-
-// do {
-//     console.log(` ${c}`)
-//     c++
-// } while ( c <= 6)
-
+// //  for(t; t > 0; t = t - 1) {
+// //     console.log( "repetindo algo ")
+// //  }
+// // console.log("ola , tudo bem?")
+// // console.log("ola , tudo bem?")
+// // console.log("ola , tudo bem?")
+// // console.log("ola , tudo bem?")
+// // console.log("ola , tudo bem?")
+// // console.log("ola , tudo bem?")
 
 
-// for de novo 
+
+
+// // var c = 1 
+// // while (c <= 6) {
+// //     console.log(`passo ${c}`)
+// //     c++
+
+// // }
+
+
+
+// // var c = 0
+
+// // do {
+// //     console.log(` ${c}`)
+// //     c++
+// // } while ( c <= 6)
+
+
+
+// // for de novo 
  
-// console.log("vai começar")
-// for (var c = 1; c <= 7; c++) {
-//     console.log(c)
+// // console.log("vai começar")
+// // for (var c = 1; c <= 7; c++) {
+// //     console.log(c)
+// // }
+// // console.log("FIM")
+
+// // console.log("inciando a contagem") 
+// // for(var o = 1; o <= 10; o++) {
+// //     console.log(o)
+// // } console.log("fim da contagem")
+
+
+// // for(let g = 20; g > 10; g--) {
+// //     console.log(`o valor de g e: ${g}`)
+// //     if(g=== 12) {
+// //      console.log("o g é 12!")
+// //      break;bra
+// //     }
+// // }
+
+
+// const job = "programador"
+
+// switch(job) {
+//     case "programador":
+//         console.log("voce é programador");
+//         break;
+//         case "advogado":
+//         console.log("voce é um advogado");
+//         break;
+//         case "jogador":
+//         console.log("voce é um jogador");
+//         break;
+//         default:
+//         console.log("profissão não encontrada");
+
 // }
-// console.log("FIM")
-
-// console.log("inciando a contagem") 
-// for(var o = 1; o <= 10; o++) {
-//     console.log(o)
-// } console.log("fim da contagem")
-
-
-// for(let g = 20; g > 10; g--) {
-//     console.log(`o valor de g e: ${g}`)
-//     if(g=== 12) {
-//      console.log("o g é 12!")
-//      break;bra
-//     }
-// }
-
-
-const job = "programador"
-
-switch(job) {
-    case "programador":
-        console.log("voce é programador");
-        break;
-        case "advogado":
-        console.log("voce é um advogado");
-        break;
-        case "jogador":
-        console.log("voce é um jogador");
-        break;
-        default:
-        console.log("profissão não encontrada");
-
-}
