eb53632deb70a3b354eb4869d77bd84094b9d3e8
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-06 11:18:07 -0300

    ..

--- ARRAYS_E_OBJETOS/js/scripts.js
+++ ARRAYS_E_OBJETOS/js/scripts.js
@@ -6,17 +6,26 @@
 
 // console.log(itens);
 
+//  mais sobre arrays
+
+const array = ["a", "b", "c", "d"];
+
+console.log(array[2]);
+
+console.log(array);
+
 //  propriedades 
 
-const numbers = [1, 2, 3]
+// const numbers = [1, 2, 3]
 
-console.log(numbers);
+// console.log(numbers);
 
-console.log(numbers.length);
+// console.log(numbers.length);
 
-console.log(numbers["length"]);
+// console.log(numbers["length"]);
 
+// const myName = "otavio"
 
-
+// console.log(myName.length)
 
 
