8c8f98980e0402718c9f6fd9b492f156a8b51136
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-07 10:26:49 -0300

    ..

--- ARRAYS_E_OBJETOS/js/scripts.js
+++ ARRAYS_E_OBJETOS/js/scripts.js
@@ -88,3 +88,23 @@
 // car.portas = 3
 
 // console.log(car)
+
+//  mais sobre obj
+
+const obj = {
+    a: "teste",
+    b: true
+}
+console.log(obj instanceof Object);
+
+
+const obj2 = {
+    c: []
+}
+
+Object.assign(obj2, obj);
+
+console.log(obj2)
+
+console.log(obj)
+
