6202365d3b1ba235b1217cbfade3e7a6883ed758
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-08 17:11:07 -0300

    ;;

--- ARRAYS_E_OBJETOS/js/scripts.js
+++ ARRAYS_E_OBJETOS/js/scripts.js
@@ -1,151 +1,162 @@
-// // const lista = [1, 2, 3, 4, 5]
-// console.log(lista);
+// // // const lista = [1, 2, 3, 4, 5]
+// // console.log(lista);
 
 
-// var itens = ["otavio", "maria meu amor", 2, 2.45]
+// // var itens = ["otavio", "maria meu amor", 2, 2.45]
 
-// console.log(itens);
+// // console.log(itens);
 
-//  mais sobre arrays
+// //  mais sobre arrays
 
-// const array = ["a", "b", "c", "d"];
+// // const array = ["a", "b", "c", "d"];
 
-// console.log(array[2]);
+// // console.log(array[2]);
 
-// console.log(array);
+// // console.log(array);
 
-//  propriedades 
+// //  propriedades 
 
-// const numbers = [1, 2, 3]
+// // const numbers = [1, 2, 3]
 
-// console.log(numbers);
+// // console.log(numbers);
 
-// console.log(numbers.length);
+// // console.log(numbers.length);
 
-// console.log(numbers["length"]);
+// // console.log(numbers["length"]);
 
-// const myName = "otavio"
+// // const myName = "otavio"
 
-// console.log(myName.length)
-
-
-// metodos
-
-// const numbers = [1, 2, 3];
-
-// const allnumbers = numbers.concat(numbers);
-
-// console.log(allnumbers);
+// // console.log(myName.length)
 
 
-//  const times = ["flamengo", "vasco", "santos"];
+// // metodos
 
-// const numbers = [1, 2, 3];
+// // const numbers = [1, 2, 3];
 
-// const timbers = times.concat(numbers)
+// // const allnumbers = numbers.concat(numbers);
 
-// console.log(timbers)
+// // console.log(allnumbers);
 
 
+// //  const times = ["flamengo", "vasco", "santos"];
+
+// // const numbers = [1, 2, 3];
+
+// // const timbers = times.concat(numbers)
+
+// // console.log(timbers)
 
 
 
-// const text = "algum texto"
-
-// console.log(text.toLocaleUpperCase())
 
 
-// const arr = ["a", "b", "c"];
+// // const text = "algum texto"
 
-// console.log(arr[2])
-
-// console.log(arr);
-// objetos 
+// // console.log(text.toLocaleUpperCase())
 
 
-// const person = {
-//     name: "otávio",
-//     age: 22,
-//     work: "programador"
+// // const arr = ["a", "b", "c"];
+
+// // console.log(arr[2])
+
+// // console.log(arr);
+// // objetos 
+
+
+// // const person = {
+// //     name: "otávio",
+// //     age: 22,
+// //     work: "programador"
+// // }
+
+// // console.log(person)
+// // console.log(person.age)
+// // console.log(person.work)
+
+
+// // const car = {
+// //     engine: 2.0,
+// //     brand: "vw",
+// //     model: "tiguan", 
+// //     km:2000 
+
+// // }
+
+
+// // console.log(car)
+
+// // car.portas = 3
+
+// // console.log(car)
+
+// //  mais sobre obj
+
+// //  const obj = {
+// //      a: "teste",
+// //      b: true
+// //  }
+// //  console.log(obj instanceof Object);
+
+// //  const obj2 = {
+// //      c: []
+// //  }
+
+// //  Object.assign(obj2, obj);
+
+// //  console.log(obj2)
+
+// // console.log(obj)
+
+// // // conhecendo melhor os objetos ...
+
+
+// // console.log(object.keys(obj));
+// // console.log(object.keys(obj2));
+
+
+// let num = [1, 2, 3, 4, 5]
+
+
+// num[5]= 6
+
+// console.log(num)
+// num.push(9)
+// console.log(num)
+// console.log(num.length)
+// console.log(num.sort)
+// console.log(num[3])
+// console.log(`o primeiro elemento é ${num [0]}`)
+
+
+
+// //  mutação 
+
+// const a = {
+//     name: "otavio"
+
 // }
 
-// console.log(person)
-// console.log(person.age)
-// console.log(person.work)
+// const b = a;
+// console.log(a);
+// console.log(b)
 
+// a.age = 22
 
-// const car = {
-//     engine: 2.0,
-//     brand: "vw",
-//     model: "tiguan", 
-//     km:2000 
+// console.log(a)
 
+// const users = ["otavio", "maria", "daniela", "isaque"];
+
+// for(let i = 0; i < users.length; i++) {
+//     console.log(`listando o usuario; ${users[i]} `)
 // }
 
 
-// console.log(car)
-
-// car.portas = 3
-
-// console.log(car)
-
-//  mais sobre obj
-
-//  const obj = {
-//      a: "teste",
-//      b: true
-//  }
-//  console.log(obj instanceof Object);
-
-//  const obj2 = {
-//      c: []
-//  }
-
-//  Object.assign(obj2, obj);
-
-//  console.log(obj2)
-
-// console.log(obj)
-
-// // conhecendo melhor os objetos ...
-
-
-// console.log(object.keys(obj));
-// console.log(object.keys(obj2));
-
-
-let num = [1, 2, 3, 4, 5]
-
-
-num[5]= 6
-
-console.log(num)
-num.push(9)
-console.log(num)
-console.log(num.length)
-console.log(num.sort)
-console.log(num[3])
-console.log(`o primeiro elemento é ${num [0]}`)
-
-
-
-//  mutação 
-
-const a = {
-    name: "otavio"
-
-}
-
-const b = a;
-console.log(a);
-console.log(b)
-
-a.age = 22
+let a = ["a", "b", "c"];
 
 console.log(a)
 
-const users = ["otavio", "maria", "daniela", "isaque"];
+a.push("d")
+console.log(a)
 
-for(let i = 0; i < users.length; i++) {
-    console.log(`listando o usuario; ${users[i]} `)
-}
+
+
