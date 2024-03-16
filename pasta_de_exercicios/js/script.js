6e1031f229e50658bd76cb264b32cb44e6f26c07
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-16 20:35:18 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -1,17 +1,18 @@
 function carregar () {
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('img')
-var data = new Date()
-var hora = data.getHours()
-var minute = data.getMinutes()
-msg.innerHTML = `agora são ${hora} horas e ${minute} minutos`
+var msg = window.document.getElementById('msg');
+var img = window.document.getElementById('img');
+var data = new Date();
+var hora = data.getHours();
+var minute = data.getMinutes();
+msg.innerHTML = `agora são ${hora} horas e ${minute} minutos`;
 if(hora >= 0 && hora < 12) {
-    img.src = 'manha.jpg'
-} else if(hora >= 12 && hora <= 18) {
-    img.src ='tarde.jpg'
+    img.src = "img/manha.jpg";
+} else if(hora >= 12 && hora < 18) {
+    img.src ="img/tarde.jpg";
 } else {
-    img.src = "noite.jpg"
+    img.src = "img/noite.jpg";
     
 }
 
 }
+carregar();
