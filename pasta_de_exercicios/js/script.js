d7e0080313649cbaa271386ebe174e42d1a873a7
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-16 13:30:17 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -1,1 +1,8 @@
-alert("ola")
+function carregar () {
+var msg = window.document.getElementById('msg')
+var imgg = window.document.getElementById('imgg')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `agora são ${hora} horas`
+if (hora)
+}
