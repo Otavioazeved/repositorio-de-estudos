4b563737a65c6de9e0899aeef279159bb06be197
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-18 16:50:22 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -1,1 +1,16 @@
-console.log("hello")
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById("txtano")
+    var res = document.getElementById("res")
+
+
+    if(fano.value.length == 0 || fano.value > ano ) {
+        window.alert("verifique os dados e tente novamente")
+    } else {
+        var fsex = document.getElementsByName ("radsex")
+        var idade = ano - fano.value
+        res.innerHTML = `idade calculada: ${idade}`
+    }
+
+}
