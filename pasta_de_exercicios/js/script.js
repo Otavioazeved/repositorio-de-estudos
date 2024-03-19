08bef9b8157032bbd54451705240af7e29fb7b18
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-19 20:22:25 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -1,21 +1,16 @@
-function contar() {
- var ini = document.getElementById('txti')
- var fim = document.getElementById('txtf')
- var passo = document.getElementById('txtp')
- var res = document.getElementById('res')
+const horas = document.getElementById("horas")
+const minutos = document.getElementById("minutos")
+const segundos = document.getElementById("segundos")
 
- if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-    window.alert("erro")
- }else{
-    res.innerHTML = 'contando: ..'
-    let i = number(ini.value)
-    let f = number(fim.value)
-    let p = number(passo.value) 
-    for (let c = i; c <= f; c += p){
-        res.innerHTML += `${c}`
-    }
-}
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let s = dateToday.getMinutes();
+    let r = dateToday.getSeconds();
+
+    horas.textContent = hr;
+    minutos.textContent = s;
+    segundos.textContent = r;
 
 
-}
-
+})
