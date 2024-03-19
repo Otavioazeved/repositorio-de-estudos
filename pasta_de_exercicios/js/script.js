24cc142d6b2d80cf579d95822ad442383a16fcaa
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-19 18:40:15 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -1,11 +1,21 @@
 function contar() {
-    let ini = document.getElementById("txti")
-    let fim = document.getElementById("txtf")
-    let passo = document.getElementById("txtp")
+ var ini = document.getElementById('txti')
+ var fim = document.getElementById('txtf')
+ var passo = document.getElementById('txtp')
+ var res = document.getElementById('res')
+
+ if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    window.alert("erro")
+ }else{
+    res.innerHTML = 'contando: ..'
+    let i = number(ini.value)
+    let f = number(fim.value)
+    let p = number(passo.value) 
+    for (let c = i; c <= f; c += p){
+        res.innerHTML += `${c}`
+    }
 }
 
-if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    window.alert("erro faltam dados")
-}else {
-    alert("tudo ok")
+
 }
+
