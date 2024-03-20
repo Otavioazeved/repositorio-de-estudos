8210aae328414418bdbba5e5b38711bb301b211d
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-20 15:16:59 -0300

    ..

--- ARRAYS_E_OBJETOS/js/scripts.js
+++ ARRAYS_E_OBJETOS/js/scripts.js
@@ -9,3 +9,12 @@ const userDetails = {
 const { firstName, lastName, Job} = userDetails;
 
 console.log(firstName, lastName, Job);
+
+
+// destructuring com arrays
+
+const myList = ["aviao", "submarino", "carro", "bike"]
+
+const [veiculoA, veiculoB, veiculoC] =  myList;
+
+console.log(veiculoA, veiculoB, veiculoC);
