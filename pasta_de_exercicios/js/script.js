7eac4746555b3e26ad93c3a1094a43f969b3fcdc
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-20 19:59:46 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -1,16 +1,17 @@
-const horas = document.getElementById('horas')
-const minutos = document.getElementById('minutos')
-const segundos = document.getElementById('segundos')
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
-    let s = dateToday.getMinutes();
-    let r = dateToday.getSeconds();
+    let minut = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
 
     horas.textContent = hr;
-    minutos.textContent = s;
-    segundos.textContent = r;
+    minutos.textContent = minut
+    segundos.textContent = sec
 
 
 })
