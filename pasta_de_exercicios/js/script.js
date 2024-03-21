d2c07e423147cbc0379e91ef0e792c31979de00f
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-20 21:04:18 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -8,6 +8,11 @@ const relogio = setInterval(function time() {
     let minut = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
+    if(hr < 10 ) hr = '0' + hr;
+
+    if(minut < 10 ) minut = '0' + minut;
+
+    if(sec < 10 ) sec = '0' + sec;
 
     horas.textContent = hr;
     minutos.textContent = minut
