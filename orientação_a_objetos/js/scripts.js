6d2d29cc587fe9dc75044b9274454944d633202d
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-25 20:22:23 -0300

    ..

--- orientação_a_objetos/js/scripts.js
+++ orientação_a_objetos/js/scripts.js
@@ -1,24 +1,42 @@
-//  1 métodos...
+// //  1 métodos...
 
-const animal = {
-    nome: "bob",
-    latir: function () {
-        console.log("au au")
-    }
+// const animal = {
+//     nome: "bob",
+//     latir: function () {
+//         console.log("au au")
+//     }
+// }
+
+// console.log(animal.nome);
+
+// animal.latir()
+
+
+// const pessoa = {
+//     nome: "otavio", 
+//     getNome: function () {
+//         return this.nome
+//     }
+   
+//     }
+    
+// console.log(pessoa.nome);
+
+// prototypes 
+
+const text = "asd"
+
+console.log(Object.getPrototypeOf(text))
+
+
+const bool = true;
+
+console.log(Object.getPrototypeOf(bool));
+
+// mais sobre prototype
+
+const myObject = {
+    a: "b"
 }
 
-console.log(animal.nome);
-
-animal.latir()
-
-
-const pessoa = {
-    nome: "otavio", 
-    getNome: function () {
-        return this.nome
-    }
-   
-    }
-    
-console.log(pessoa.nome);
-
+console.log(Object.getPrototypeOf(myObject))
