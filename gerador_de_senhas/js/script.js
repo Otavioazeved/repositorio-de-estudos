768676de78a16d37f8bb2041c9e1052669d0f733
otavio azevedo <otavioazevedo39@gmail.com>
2024-03-25 15:45:33 -0300

    ..

--- gerador_de_senhas/js/script.js
+++ gerador_de_senhas/js/script.js
@@ -3,7 +3,7 @@ let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
-let containerPassword = document.querySelector("#container-password");
+let containerPassword = document.querySelector("#container-Password");
 
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVXYZ123456789!@";
@@ -20,8 +20,9 @@ function generatePassword() {
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random * n))
     }
+    containerPassword.classList.remove('hide');
+    password.innerHTML = pass;
 }
 
-containerPassword.classList.remove("hide");
-password.innerHTML = pass;
+
 
