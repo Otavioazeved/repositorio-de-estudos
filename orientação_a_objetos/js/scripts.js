0ecce3948ea424a1486b110469cdfea29ac44868
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-01 14:59:54 -0300

    ..

--- orientação_a_objetos/js/scripts.js
+++ orientação_a_objetos/js/scripts.js
@@ -75,56 +75,56 @@
 
 
 
-function criarCachorro(nome, raca) {
-    const cachorro = Object.create({})
-    cachorro.nome = nome
-    cachorro.raca = raca
-     return cachorro 
-     }
+// function criarCachorro(nome, raca) {
+//     const cachorro = Object.create({})
+//     cachorro.nome = nome
+//     cachorro.raca = raca
+//      return cachorro 
+//      }
 
-console.log(criarCachorro)
-
-
- const bob = criarCachorro("bob", "vira lata");
-
- console.log(bob);
-
- const jack = criarCachorro("jack", "pitbull");
-
- console.log(jack);
+// console.log(criarCachorro)
 
 
-//  funções construtoras usando new 
-// palavra chave new usado para instaciar um novo objeto 
-// objeto sendo criado por meio de uma função, ja entrega o objeto pronto
+//  const bob = criarCachorro("bob", "vira lata");
 
-function Cachorro (nome, raca) {
-    this.nome = nome
-    this.raca = raca
+//  console.log(bob);
 
-}
+//  const jack = criarCachorro("jack", "pitbull");
+
+//  console.log(jack);
 
 
-const husky = new Cachorro("ozzy", "husky")
+// //  funções construtoras usando new 
+// // palavra chave new usado para instaciar um novo objeto 
+// // objeto sendo criado por meio de uma função, ja entrega o objeto pronto
+
+// function Cachorro (nome, raca) {
+//     this.nome = nome
+//     this.raca = raca
+
+// }
 
 
-console.log(husky)
-
-// instanciar é basicamente criar um objeto a base de uma função especifica...
+// const husky = new Cachorro("ozzy", "husky")
 
 
+// console.log(husky)
 
-function Escola(nome, alunos, professores) {
-    this.nome = nome;
-    this.alunos = alunos;
-    this.professores = professores;
-
-}
+// // instanciar é basicamente criar um objeto a base de uma função especifica...
 
 
-const saida = new Escola("Escola nova vida", "204 alunos", "35 professores");
 
-console.log(saida);
+// function Escola(nome, alunos, professores) {
+//     this.nome = nome;
+//     this.alunos = alunos;
+//     this.professores = professores;
+
+// }
+
+
+// const saida = new Escola("Escola nova vida", "204 alunos", "35 professores");
+
+// console.log(saida);
 
 
 // classes de funções com metódos
@@ -138,17 +138,42 @@ console.log(saida);
 
 
 
-function Carro(nome, modelo, kilometragem, cor, combustivel ) {
-    this.nome = nome;
-    this.modelo = modelo;
-    this.kilometragem = kilometragem;
-    this.cor = cor;
-    this.combustivel = combustivel;
+//  function Carro(nome, modelo, kilometragem, cor, combustivel ) {
+//      this.nome = nome;
+//      this.modelo = modelo;
+//      this.kilometragem = kilometragem;
+//      this.cor = cor;
+//     this.combustivel = combustivel;
+// }
+// const meuCarro = new Carro("corsa", "chevrolet", "160milkm", "preto", "gnv")
+// console.log(meuCarro)
+
+
+
+// function Facul(nome, ano, alunos, cursos) {
+
+//     this.nome = nome;
+//     this.ano = ano ;
+//     this.alunos = alunos;
+//     this.cursos = cursos;
+// }
+
+
+// const faculdade = new Facul("rural", 1942, "240 alunos", "24 cursos de graduação")
+
+//  console.log(faculdade)
+
+// classes ES6....
+//  versões mais novas do javascript, abandonamos as funtios e usamos classes
+// ha recursos comuns como constructor alem da insatncia new...
+
+
+
+class cachorroClasse {
+    constructor(nome, raca)
+    this,raca 
 }
 
-const meuCarro = new Carro("corsa", "chevrolet", "160milkm", "preto", "gnv")
-
-console.log(meuCarro)
 
 
 
