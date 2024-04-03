4ff9e10280c9f04f63c88af543dec20bcf7ad945
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-02 22:21:24 -0300

    ..

--- orientação_a_objetos/js/scripts.js
+++ orientação_a_objetos/js/scripts.js
@@ -1,38 +1,47 @@
-// ES6 JS 
+// // ES6 JS 
 
-class CachorroClasse {
-    constructor (nome, raca) {
-        this.nome = nome
-        this.raca = raca
+// class CachorroClasse {
+//     constructor (nome, raca) {
+//         this.nome = nome
+//         this.raca = raca
     
+//     }
+// }
+
+// const jeff = new CachorroClasse("jeff", "puddle")
+// console.log(jeff)
+
+// // MAIS SOBRE CLASSES
+
+// class Caminhao {
+//     constructor (nome, cor) { 
+//     this.nome = nome
+//     this.cor = cor 
+
+
+//     }
+
+//     descreverCaminhao () {
+//         console.log(`este caminhao tem o nome ${this.nome} e é da cor ${this.cor}`);
+//     }
+// }
+//  const minhao = new Caminhao ("olavo", "verde") ;
+
+//  console.log(minhao);
+
+//  minhao.descreverCaminhao();
+
+
+ class Ytube {
+    constructor(ano, canais, lives) {
+        this.ano = ano;
+        this.canais = canais;
+        this.lives = lives;
     }
-}
+ }
 
-const jeff = new CachorroClasse("jeff", "puddle")
-console.log(jeff)
-
-// MAIS SOBRE CLASSES
-
-class Caminhao {
-    constructor (nome, cor) { 
-    this.nome = nome
-    this.cor = cor 
-
-
-    }
-
-    descreverCaminhao () {
-        console.log(`este caminhao tem o nome ${this.nome} e é da cor ${this.cor}`);
-    }
-}
- const minhao = new Caminhao ("olavo", "verde") ;
-
- console.log(minhao);
-
- minhao.descreverCaminhao();
-
-
-
+const chanel = new Ytube (1998, "2450 canais de yt", "13000 mil lives simultaneas")
+console.log(chanel)
 
 
 
