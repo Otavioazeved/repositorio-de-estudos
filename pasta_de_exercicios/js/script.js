9de059ea7cc1dd1fea39ea15a4e3169453203fa9
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-03 20:56:22 -0300

    ..

--- pasta_de_exercicios/js/script.js
+++ pasta_de_exercicios/js/script.js
@@ -2,14 +2,20 @@ function tabuada() {
     let num = document.getElementById("txtn")
     let tab = document.getElementById("seltab")
 
-if(num.value.length == 0) {
+    if(num.value.length === 0) {
        window.alert("por favor digite um numero")
-} else {
+}   else {
     let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ""
+    while(c <= 10) {
+        let item = document.createElement("option")
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        c++
+    }
 }
 
-
-
-
-   
-}
+     
+    
+    }
