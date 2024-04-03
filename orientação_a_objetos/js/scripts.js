acaf48e4241486e7070e235ca3cfe56765905a6d
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-03 18:59:15 -0300

    ..

--- orientação_a_objetos/js/scripts.js
+++ orientação_a_objetos/js/scripts.js
@@ -1,81 +1,96 @@
-// // ES6 JS 
+// // // ES6 JS 
 
-// class CachorroClasse {
-//     constructor (nome, raca) {
-//         this.nome = nome
-//         this.raca = raca
+// // class CachorroClasse {
+// //     constructor (nome, raca) {
+// //         this.nome = nome
+// //         this.raca = raca
     
-//     }
-// }
+// //     }
+// // }
 
-// const jeff = new CachorroClasse("jeff", "puddle")
-// console.log(jeff)
+// // const jeff = new CachorroClasse("jeff", "puddle")
+// // console.log(jeff)
 
-// // MAIS SOBRE CLASSES
+// // // MAIS SOBRE CLASSES
 
-// class Caminhao {
-//     constructor (nome, cor) { 
-//     this.nome = nome
-//     this.cor = cor 
-
-
-//     }
-
-//     descreverCaminhao () {
-//         console.log(`este caminhao tem o nome ${this.nome} e é da cor ${this.cor}`);
-//     }
-// }
-//  const minhao = new Caminhao ("olavo", "verde") ;
-
-//  console.log(minhao);
-
-//  minhao.descreverCaminhao();
+// // class Caminhao {
+// //     constructor (nome, cor) { 
+// //     this.nome = nome
+// //     this.cor = cor 
 
 
-//  class Ytube {
-//     constructor(ano, canais, lives) {
-//         this.ano = ano;
-//         this.canais = canais;
-//         this.lives = lives;
-//     }
-//  }
+// //     }
 
-// const chanel = new Ytube (1998, "2450 canais de yt", "13000 mil lives simultaneas")
-// console.log(chanel)
+// //     descreverCaminhao () {
+// //         console.log(`este caminhao tem o nome ${this.nome} e é da cor ${this.cor}`);
+// //     }
+// // }
+// //  const minhao = new Caminhao ("olavo", "verde") ;
+
+// //  console.log(minhao);
+
+// //  minhao.descreverCaminhao();
 
 
-// override nas prop via prototype (substituição quando tem um valor e troca pelo outro)  instancias baseadas nas classe, 
+// //  class Ytube {
+// //     constructor(ano, canais, lives) {
+// //         this.ano = ano;
+// //         this.canais = canais;
+// //         this.lives = lives;
+// //     }
+// //  }
 
-// class Humano {
-//     constructor(idade, nome) {
-//         this.idade = idade;
+// // const chanel = new Ytube (1998, "2450 canais de yt", "13000 mil lives simultaneas")
+// // console.log(chanel)
+
+
+// // override nas prop via prototype (substituição quando tem um valor e troca pelo outro)  instancias baseadas nas classe, 
+
+// // class Humano {
+// //     constructor(idade, nome) {
+// //         this.idade = idade;
+// //         this.nome = nome;
+    
+
+// //     }
+// // }
+
+// // const otavio = new Humano(22, "otavio")
+// // console.log(otavio)
+
+
+// class Animal {
+//     constructor(nome, idade, sexo){
 //         this.nome = nome;
-    
+//         this.idade = idade;
+//         this.sexo = sexo;
 
 //     }
 // }
 
-// const otavio = new Humano(22, "otavio")
-// console.log(otavio)
+// const MeuAnimal = new Animal("leopardo", 6, "masculino")
+
+// console.log(MeuAnimal)
 
 
-class Animal {
-    constructor(nome, idade, sexo){
-        this.nome = nome;
-        this.idade = idade;
-        this.sexo = sexo;
+// Animal.prototype.idade = "não definida"
+// console.log(MeuAnimal.idade)
+// console.log(Animal.prototype.idade)
 
-    }
+// symbols com classes, não estao atrelados eles podem ser usados sem classes
+// possivel criar uma prop única e imutavel, util quando ha algum dado que se repetira em todos os objetos criados a partir da classe.
+// symbols são unicos e imutaveis, ou seja quando eu crio um ele não pode ser alterado
+// propriedade que a gente nao quer correr o risco de que tenha um conlitos de nomes...
+
+class Aviao {
+    constructor(marca, turbinas) [
+        this.marca = marca;
+        this.turbinas = turbinas;
+    ]
 }
-
-const MeuAnimal = new Animal("leopardo", 6, "masculino")
-
-console.log(MeuAnimal)
+ 
 
 
-Animal.prototype.idade = "não definida"
-console.log(MeuAnimal.idade)
-console.log(Animal.prototype.idade)
 
 
 
