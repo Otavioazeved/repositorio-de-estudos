6e8ad2ee8a17331d3642693851fa59d86c426fef
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-04 19:57:14 -0300

    ..

---
+++ analisador_de_num/js/script.js
@@ -0,0 +1,14 @@
+let num = document.querySelector("input#numx")
+let lista = document.querySelector("select#flista")
+let res = document.querySelector("div#res")
+
+let valores = []
+
+
+
+function adicionar() {
+    if(isnumero(num.value) && !inlista(num.value)) 
+
+} else{
+    window.alert("valor não encontrado ou ja encontrado na lista")
+}
