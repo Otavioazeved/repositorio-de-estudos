35c1fe3f2390781e935ee66f4ed530fe710e05c4
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-13 19:31:33 -0300

    .

---
+++ deupuração_js/js/script.js
@@ -0,0 +1,4 @@
+// debug e tratamento de erros forma d eidentificar como o codigo esta progredindo
+// o que é bug e debug?
+// debug método de encontrar e resolver o bug tem estrategias pra resolver isso
+// 
