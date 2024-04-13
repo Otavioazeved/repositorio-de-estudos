08099b29d24ef9e64632fafc594bc3874d7672c9
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-13 19:46:44 -0300

    ..

--- deupuração_js/js/script.js
+++ deupuração_js/js/script.js
@@ -1,4 +1,13 @@
 // debug e tratamento de erros forma d eidentificar como o codigo esta progredindo
 // o que é bug e debug?
 // debug método de encontrar e resolver o bug tem estrategias pra resolver isso
-// 
+// validação.... pode ter p quem desenvolvendo o software ou p quem está utilizando o software vulgo o cliente
+// strict mode modo que deixa o js mais rigoroso na hora de programar, declarado no topo de arquivo de funções, os strict n limita os recursos js 
+
+
+// strict.....
+// strict é um conserta programador, fica me mostrando td que to errando
+
+"use strict";
+
+const undefined = "teste"
