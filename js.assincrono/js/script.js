55abfd79d08d17fac34453bf8b71f8b5b99dbb37
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-24 19:45:04 -0300

    .

---
+++ js.assincrono/js/script.js
@@ -0,0 +1,13 @@
+// js assincrono...  utilizada quando as respostas não são obtidas de forma instantanea no programa 
+// na programação assincrona as execuções não ocorrem em formato de fila, e sim no seu tempo 
+
+// setTimeout... prog assincrona estabelece uma ação para ser executada após um certo tempo 
+
+// o tempo é milesimos de segundos     3000 é 3 segundos exemplos
+
+
+
+
+setTimeout(function () {
+    document.querySelector('#text').innerText = 'hello word'
+}, 1000)
