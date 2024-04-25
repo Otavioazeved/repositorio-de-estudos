27e630cc39973a9978bafcd0463bac294dcce043
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-25 10:25:06 -0300

    .

--- js.assincrono/js/script.js
+++ js.assincrono/js/script.js
@@ -9,19 +9,37 @@
 // setTimeout(funcao, tempo);
 
 
-function ativarContagem() {
-    document.getElementById("tempo").innerHTML = "começou a contar"
-    setTimeout(function(){
-document.getElementById("tempo").innerHTML = "executou o setTimeout"
-    },5000)
-}
-
+// function ativarContagem() {
+//     document.getElementById("tempo").innerHTML = "começou a contar"
+//     setTimeout(function(){
+// document.getElementById("tempo").innerHTML = "executou o setTimeout"
+//     },5000)
 // }
 
+// // }
 
- setTimeout(function(){
-    console.log("hello")
- }, 3000)
+
+//  setTimeout(function(){
+//     console.log("hello")
+//  }, 3000)
+
+
+
+
+
+
 // timeout ativa a função apenas 1 vez quando der o tempo especificado 
 
+// setInterval... semelhante a setTimeout porém ela NÃO para de ser executada é como um loop infinito com execução de tempo controlada
 
+
+// setInterval
+
+  console.log("ainda não comecou")
+
+
+setInterval(function () {
+    console.log("intervalo assincrono")
+}, 3000)
+
+console.log("ainda não começou 2")
