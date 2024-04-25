856a33255539d973595f3f69d864dd6c64020350
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-25 12:03:13 -0300

    .

--- js.assincrono/js/script.js
+++ js.assincrono/js/script.js
@@ -62,7 +62,15 @@ function ativarContagem() {
    tempo = setInterval(function(){
     var cronometro = document.getElementById("tempo").innerHTML;
     var soma = parseInt (cronometro) - 1;
+
+    if(soma === 0 ){
+        document.getElementById("tempo").innerHTML = "tempo esgotado"
+      pararContagem ()
+    }else {
+        document.getElementById("tempo").innerHTML = soma
+    }
     document.getElementById("tempo").innerHTML = soma
+
 }, 1000)
 
 
@@ -75,6 +83,7 @@ function ativarContagem() {
 
 function pararContagem () {
     clearInterval(tempo);
+    document.getElementById("tempo").innerHTML = "tempo esgotado"
 }
 
 
