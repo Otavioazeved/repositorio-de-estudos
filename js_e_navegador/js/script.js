674b0bef54c449cdacbfe28a8818ee8a0c2cb7d7
otavio azevedo <otavioazevedo39@gmail.com>
2024-05-13 11:14:43 -0300

    .

---
+++ js_e_navegador/js/script.js
@@ -0,0 +1,19 @@
+/*
+Protocolos da web
+
+HTTP: serve para solicitar ARQUIVOS e IMAGENS do servidor (hyper transfer protocol)
+
+SMTP: protocolo para envio de email.
+
+
+TCP: utilizado para transferencia de dados
+
+URL: cada arquivo que é carregado no navegador tem uma url 
+
+
+DOM: representação fiel do html da pagina, quando cria um script de js e linka no html a gente tem disponivel o DOM 
+atraves dele tbm podemos atrelar eventos como click ou pressionar teclas de mouse
+
+*/
+
+
