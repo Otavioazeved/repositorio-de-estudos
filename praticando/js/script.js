cbf2bee99567b8f88a4c6e28604d1c632bdcc2ab
otavio azevedo <otavioazevedo39@gmail.com>
2024-06-11 20:58:24 -0300

    .

--- praticando/js/script.js
+++ praticando/js/script.js
@@ -34,3 +34,39 @@ let NomeCompleto = nome + " " +sobrenome
 
 
 console.log("nome completo:", NomeCompleto)
+
+// soma
+
+let a = 90
+let b = 9
+
+let res = a - b
+
+console.log("A resposta é:", res)
+
+
+let c = 1000
+let d = 100
+let e = c/d
+
+console.log("A divisão entre c e d é:", e)
+
+
+// boleanos.
+
+
+
+let bool1 = true
+let bool2 = false
+  
+
+console.log("and:", bool1 && bool2)
+console.log("or:", bool1 || bool2)
+console.log("not:", !bool1)
+
+
+
+
+
+
+
