193b5cc935b7251c63903deab2b5c040a475f666
otavio azevedo <otavioazevedo39@gmail.com>
2024-06-11 20:37:44 -0300

    .

---
+++ praticando/js/script.js
@@ -0,0 +1,36 @@
+//  exercicio 1-
+// variveis, tipos de dados e operadores.
+
+
+const idade = 10
+const peso = 20
+
+console.log("idade:", idade)
+console.log("peso:", peso)
+
+
+const pi = "3,14"
+
+console.log("pi:", pi)
+
+
+const NumeroMaximo = Number.MIN_VALUE
+
+
+console.log("NumeroMaximo:", NumeroMaximo)
+
+
+// const nome = "otavio"
+// const sobrenome = "azevedo"
+
+// console.log("nome:", nome  +  "sobrenome:", sobrenome)
+
+
+// correção
+
+let nome = "otavio"
+let sobrenome = "azevedo"
+let NomeCompleto = nome + " " +sobrenome
+
+
+console.log("nome completo:", NomeCompleto)
