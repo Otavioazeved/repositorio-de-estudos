5bc48e6ab4548db18ae2bda9571dac6981455719
otavio azevedo <otavioazevedo39@gmail.com>
2024-06-12 11:51:17 -0300

    ..

--- praticando/js/script.js
+++ praticando/js/script.js
@@ -1,100 +1,171 @@
-//  exercicio 1-
-// variveis, tipos de dados e operadores.
+// //  exercicio 1-
+// // variveis, tipos de dados e operadores.
 
 
-const idade = 10
-const peso = 20
+// const idade = 10
+// const peso = 20
 
-console.log("idade:", idade)
-console.log("peso:", peso)
-
-
-const pi = "3,14"
-
-console.log("pi:", pi)
+// console.log("idade:", idade)
+// console.log("peso:", peso)
 
 
-const NumeroMaximo = Number.MIN_VALUE
+// const pi = "3,14"
+
+// console.log("pi:", pi)
 
 
-console.log("NumeroMaximo:", NumeroMaximo)
+// const NumeroMaximo = Number.MIN_VALUE
 
 
-// const nome = "otavio"
-// const sobrenome = "azevedo"
-
-// console.log("nome:", nome  +  "sobrenome:", sobrenome)
+// console.log("NumeroMaximo:", NumeroMaximo)
 
 
-// correção
+// // const nome = "otavio"
+// // const sobrenome = "azevedo"
 
-let nome = "otavio"
-let sobrenome = "azevedo"
-let NomeCompleto = nome + " " +sobrenome
-
-
-console.log("nome completo:", NomeCompleto)
-
-// soma
-
-// let a = 90
-// let b = 9
-
-// let res = a - b
-
-// console.log("A resposta é:", res)
+// // console.log("nome:", nome  +  "sobrenome:", sobrenome)
 
 
-let c = 1000
-let d = 100
-let e = c/d
+// // correção
 
-console.log("A divisão entre c e d é:", e)
-
-
-// boleanos.
-// || siginifica "ou" ai le assim x valor ou y valor é verdaderio?se um dos dois for verdaderio então é true  
-// && significa "e", ai ler assim x e y são verdadeiros ai nessa pdoeria dar falso pois um dos dois pode ser falso
-// not "!" siginifica "não" ai le assim, x não verdadeiro e se ele for verdadeiro vai da false 
+// let nome = "otavio"
+// let sobrenome = "azevedo"
+// let NomeCompleto = nome + " " +sobrenome
 
 
-let bool1 = true
-let bool2 = false
+// console.log("nome completo:", NomeCompleto)
+
+// // soma
+
+// // let a = 90
+// // let b = 9
+
+// // let res = a - b
+
+// // console.log("A resposta é:", res)
+
+
+// let c = 1000
+// let d = 100
+// let e = c/d
+
+// console.log("A divisão entre c e d é:", e)
+
+
+// // boleanos.
+// // || siginifica "ou" ai le assim x valor ou y valor é verdaderio?se um dos dois for verdaderio então é true  
+// // && significa "e", ai ler assim x e y são verdadeiros ai nessa pdoeria dar falso pois um dos dois pode ser falso
+// // not "!" siginifica "não" ai le assim, x não verdadeiro e se ele for verdadeiro vai da false 
+
+
+// let bool1 = true
+// let bool2 = false
   
 
-console.log("and:", bool1 && bool2)
-console.log("or:", bool1 || bool2)
-console.log("not:", !bool1)
+// console.log("and:", bool1 && bool2)
+// console.log("or:", bool1 || bool2)
+// console.log("not:", !bool1)
 
-//  operadores de comparação
-// == é pra saber se é igual
-let a = 5
-let b = 3
-console.log( " a resposta é:", a < b)
-console.log("a resposta é:", a == b)
-console.log("a resposta é:", a > b)
-
-
-
-// INCREMENTO             (ISSO AQUI É IMPORTANTE PRA CARALHO IRMÃO)
-
-// aqui ele pode aumentar ou diminuir seus valores entendeu, exemplo num++ ele soma mais um, num-- ele diminiu 
+// //  operadores de comparação
+// // == é pra saber se é igual
+// let a = 5
+// let b = 3
+// console.log( " a resposta é:", a < b)
+// console.log("a resposta é:", a == b)
+// console.log("a resposta é:", a > b)
 
 
-let num = 10;
-num--;
 
-console.log("incremento:", num )
+// // INCREMENTO             (ISSO AQUI É IMPORTANTE PRA CARALHO IRMÃO)
+
+// // aqui ele pode aumentar ou diminuir seus valores entendeu, exemplo num++ ele soma mais um, num-- ele diminiu 
 
 
-let number = 124;
-number++
+// let num = 10;
+// num--;
 
-console.log("o numero somado é:", number)
+// console.log("incremento:", num )
+
+
+// let number = 124;
+// number--
+
+// console.log("o numero somado é:", number)
 
 
 
 // operadores condicionais
+
+
+function verificarNumero (num){
+    if(num > 0) {
+        console.log("Numero positivo")
+    } else if(num < 0){
+        console.log("numero negativo")
+    }else{
+        console.log("numero é zero")
+    }
+}
+
+
+
+
+verificarNumero(10)
+verificarNumero(-9)
+
+
+// else if usado depois de um if para testar outra condição somente se a if for falsa
+// else não testa nenhum executa o codigo quando nenhuma das cond são verdadeira if ou else if
+
+
+function verificarCNH (num){
+    if( num >= 18){
+        console.log("voce tem idade para dirigir e pode tirar sua cnh")
+    }else if(num < 12 && num < 18){
+        console.log("voce não tem idade para tirar sua cnh, é um adolescente va ja brincar euem")
+    }else{
+        console.log("voce é um adulto")
+    }
+    
+}
+
+verificarCNH(21)
+verificarCNH(9)
+verificarCNH()
+
+
+
+ MediaAluno = (notaA, notaB) => {
+    let a = notaA
+    let b = notaB
+    
+
+    if((notaA + notaB)/2 >= 7){
+        console.log("aluno aprovado")
+    }else if((notaA + notaB)/2 <= 7){
+        console.log("aluno reprovado")
+    }
+}
+
+MediaAluno(9,3)
+
+
+
+VerificarVoto = (obt, facul) => {
+    let a = obt
+    let b = facul
+    if(obt >= 18 && obt <= 70){
+        console.log("Voto obrigatório")
+    }else if (facul <= 17 && facul >= 71 ){
+        console.log("voto facultativo")
+    }else {
+        (console.log("voce não tem idade para votar"))
+    }
+}
+
+
+VerificarVoto(0)
+
 
 
 
