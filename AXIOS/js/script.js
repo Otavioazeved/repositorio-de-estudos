2647326ececa8f38cc6608ba8bf19bbcd71503a0
otavio azevedo <otavioazevedo39@gmail.com>
2024-07-23 15:25:21 -0300

    .

--- AXIOS/js/script.js
+++ AXIOS/js/script.js
@@ -62,17 +62,17 @@ criar elementos baseados no que veio na requisição.
 // printData();
 
 // como consumir um api c axios e js puro.
-var nameusuarioo =  document.querySelector("#nomeUsuario")
+const nameusuarioo =  document.querySelector("#nomeUsuario")
 function trazerRepositorios(){
     axios
     .get(`https:/api.github.com/users/${nomeUsuario.value}/repos`)
     .then((response) =>{
         const repos = response.data
         console.log(repos)
-        var lista = document.querySelector("#repos");
+        const lista = document.querySelector("#repos");
         for(i in repos){
             console.log(repos[i])
-            var elemento = document.querySelector("#li")
+            const elemento = document.querySelector("#li")
             elemento.innerHTML = JSON.stringify(repos[i].url)
 
             lista.appendChild(elemento)
